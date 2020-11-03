6069b6e3438e76f77df9608c17c60db55fa55962
CJUSTNOW <2414964936@qq.com>
2020-11-03 16:00:27 +0800

    选择内网穿透

--- src/network/request.js
+++ src/network/request.js
@@ -4,8 +4,8 @@ export function request(config) {
   return new Promise((resolve, reject) => {
     const instance = axios.create({
       // baseURL: 'http://152.136.185.210:8000/api/w6',
-      baseURL: 'http://192.168.16.127:3000',
-      // baseURL: 'https://9e42dql8.dongtaiyuming.net/',
+      // baseURL: 'http://192.168.16.127:3000',
+      baseURL: 'https://9e42dql8.dongtaiyuming.net/',
      timeout: 5000
     })
 
